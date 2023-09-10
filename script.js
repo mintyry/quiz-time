42f8dc4410eee1d02e73cd368b568866c804fd62
mintyry <gitmintyry@gmail.com>
2023-09-10 00:23:23 -0700

    added comments to js

--- script.js
+++ script.js
@@ -1,111 +1,123 @@
 //starting off with knowns/made variables, arrays, questions
 let timer = document.querySelectorAll('.countdown');
 let startBtn = document.querySelector('#surroundHoot');
+//variables in question/quiz portion
 let quiz = document.querySelector('#question');
-let multChoice = document.querySelector('#choices');
+let choiceSection = document.querySelector('#choices');
 let currentIndex = 0;
-
+//array of questions and choices
 let questions = [
-    {
-      title: 'What does Pokémon stand for?',
-      choices: ['Poke Mobile On', 'Pocket Monsters', 'Pocket Monastery', 'For what is right in the world'],
-      answer: 'Pocket Monsters',
-    },
-    {
-      title: 'Who is the mascot of the Pokémon franchise?',
-      choices: ['Charizard', 'Mewtwo', 'Pikachu', 'Muk'],
-      answer: 'Pikachu',
-    },
-    {
-      title: 'What was the name of the lead protagonist in the Japanese Pokémon anime?',
-      choices: [
-        'Satoshi',
-        'Ash',
-        'Red',
-        'Suzuki',
-      ],
-      answer: 'Satoshi',
-    },
-    {
-      title:
-        'Which of these is a fire type Pokémon weak to?',
-      choices: ['Water', 'Rock', 'Ground', 'All of the above'],
-      answer: 'All of the above',
-    },
-    {
-      title:
-        'Which type would be best to use against a Dark Tera-type Mewtwo?',
-      choices: ['Fighting', 'Bug', 'Fairy', 'Poison'],
-      answer: 'Bug',
-    },
-  ];
+  {
+    title: '1. What does Pokémon stand for?',
+    choices: ['Poke Mobile On', 'Pocket Monsters', 'Pocket Monastery', 'For what is right in the world'],
+    answer: 'Pocket Monsters',
+  },
+  {
+    title: '2. Who is the mascot of the Pokémon franchise?',
+    choices: ['Charizard', 'Mewtwo', 'Pikachu', 'Muk'],
+    answer: 'Pikachu',
+  },
+  {
+    title: '3. What was the name of the lead protagonist in the Japanese Pokémon anime?',
+    choices: [
+      'Satoshi',
+      'Ash',
+      'Red',
+      'Suzuki',
+    ],
+    answer: 'Satoshi',
+  },
+  {
+    title:
+      '4. Which of these is a fire type Pokémon weak to?',
+    choices: ['Water', 'Rock', 'Ground', 'All of the above'],
+    answer: 'All of the above',
+  },
+  {
+    title:
+      '5. Which type would be best to use against a Dark Tera-type Mewtwo?',
+    choices: ['Fighting', 'Bug', 'Fairy', 'Poison'],
+    answer: 'Bug',
+  },
+];
 //just to test how to access
-  console.log(questions);
-  console.log(questions[1].title);
-  console.log(questions[1].choices);
-  console.log(questions[1].answer);
+console.log(questions);//all of the questions/choices in array
+console.log(questions[0].title);//access title of [index] question
+console.log(questions[0].choices); //access choices ""
+console.log(questions[0].answer); //access answer ""
 
 //code for timer
-function countdown () {
+function countdown() {
   let timeLeft = 60;
   let timeInterval = setInterval(function () {
-      timer[0].textContent = timeLeft;
-      timer[1].textContent = timeLeft;
-      timeLeft--;
-      if (timeLeft === 0) {
-          clearInterval(timeInterval);
-          //gonna need a time's up message
-          timer[0].textContent = 'Time\'s up!';
-          timer[1].textContent = 'You\'re done!';
-          timer[0].setAttribute('style', 'font-size: 7vh');
-          timer[1].setAttribute('style', 'font-size: 6.5vh');
-      }
+    timer[0].textContent = timeLeft;
+    timer[1].textContent = timeLeft;
+    timeLeft--;
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      //gonna need a time's up message
+      timer[0].textContent = 'Time\'s up!';
+      timer[1].textContent = 'You\'re done!';
+      timer[0].setAttribute('style', 'font-size: 7vh');
+      timer[1].setAttribute('style', 'font-size: 6.5vh');
+    }
   }, 1000)
 };
 
-  //code for showing questions/choices
+//make reusable code (ie: function) that will show question and choices
+//putting content inside of quiz variable (accesses #question in HTML)
+//the content will override what's in HTML and become the various questions, accessed by their index number
+//choiceSection (which accesses the ul #choices, is an empty section for the multiple choices that will populate from the for loop)
+//when this for loop runs, it will create list items containing buttons in the ul
+//populate empty buttons with the choices for the question
+//onclick, runs nextQuestion function which will increase the index number for the questions array, thus changing to the next question.
+function showQuiz() {
+  quiz.textContent = questions[currentIndex].title;
+  choiceSection.textContent = '';
 
-  //make reusable code that will show question and choices
-  function showQuiz () {
-    //putting content inside of quiz variable (accesses #question in HTML)
-    //the content will be the various questions, accessed by their index number
-    quiz.textContent = questions[currentIndex].title;
-    multChoice.textContent = '';
+  for (let i = 0; i < questions[currentIndex].choices.length; i++) {
 
-    for (let i = 0; i < questions[currentIndex].choices.length; i++) {
-      let listEl = document.createElement('li');
-      let listBtn = document.createElement('button');
-      listBtn.textContent = questions[currentIndex].choices[i];
-      listBtn.onclick = function () {
-        nextQuestion();
-      };
-      listEl.appendChild(listBtn);
-      multChoice.appendChild(listEl);
-    }
+    let listEl = document.createElement('li');
+    let listBtn = document.createElement('button');
 
-    
-  };
+    listBtn.textContent = questions[currentIndex].choices[i];
 
-  function nextQuestion () {
-    currentIndex++;
-    showQuiz();
-  };
+    listBtn.onclick = function () {
+      nextQuestion();
+    };
+    listEl.appendChild(listBtn);
+    choiceSection.appendChild(listEl);
+  }
+};
+
+function nextQuestion() {
+  currentIndex++;
+  showQuiz();
+  
+};
 
 
 //code for clicking start button
-//maybe wrap this in if statement later so the else can be when user clicks again, it presents next question
-startBtn.addEventListener("click", function() {
 //call timer
-  countdown();
 //call questions
+//disables button from being clicked after first click
+
+startBtn.addEventListener("click", function () {
+  countdown();
   showQuiz();
   startBtn.setAttribute('disabled', true);
 });
 
+// listBtn.addEventListener("click", function() {
+//   if (i >= questions[currentIndex].choices.length) {
+//     quiz.textContent = 'Leaderboards';
+//     choiceSection = 'test name';
+//   }
+// } );
 
 
 
-  
 
 
-  
+
+
