b29a130cb2fa6c3f6d5076741fd37d79457b81a0
mintyry <gitmintyry@gmail.com>
2023-09-11 15:39:49 -0700

    added set attribute for if button clicked is wrong, color of button should change. doesnt work

--- script.js
+++ script.js
@@ -107,6 +107,7 @@ function nextQuestion() {
 function checkCorrect (x, y) {
   if (x !== y) {
     timeLeft = timeLeft - 10;
+    // listBtn.setAttribute('style','background-color:red');
   };
   nextQuestion();
 };
